cda6af1b9deeb6db23b60cb42efc6c630ea9303e
Joseph Klemen <joe.klemen@qualia.com>
2023-12-12 00:23:25 -0500

    Day 09

---
+++ 2023/09_mirage_maintenance/part1.rb
@@ -0,0 +1,26 @@
+def expand(sequence, l = 1)
+  if l == sequence.length
+    return sequence
+  end
+
+  all_zero = true
+  (0...sequence.length-l).map do |i|
+    sequence[i] = sequence[i + 1] - sequence[i]
+    all_zero &&= sequence[i] == 0
+  end
+
+  if !all_zero
+    expand(sequence, l + 1)
+  end
+
+  sequence.sum
+end
+
+lines = File.open(ARGV[0], 'r', &:readlines)
+
+result = lines.reduce(0) do |sum, line|
+  sequence = line.chomp.split(' ').map(&:to_i)
+  sum + expand(sequence)
+end
+
+p result
